0f61115900bb8d6adec06eaa75a09e85aacde67b
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-30 17:53:38 -0300

    Validação de numeros pares

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,2 +1,9 @@
 const numeros = [0, 122, 4, 6, 7, 8, 44];
 
+const saoPares = numeros.every(x => x % 2 == 0);
+
+if (saoPares) {
+    console.log("Array válido");
+} else {
+    console.log("Array inválido");
+}
