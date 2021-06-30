f3072766a0775973e94215741be29006e9146ea2
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-30 17:41:23 -0300

    Verificando limite de caracters

--- classe-04/index.js
+++ classe-04/index.js
@@ -1,1 +1,9 @@
 const palavras = ['livro', 'caneta', 'sol', 'carro', 'orelha'];
+
+const invalido = palavras.some(x => x.length > 5);
+
+if (invalido) {
+    console.log("Existe palavra inválida");
+} else {
+    console.log("Array validado");
+}
