9155708f87a22b96e3e598a8436818bf6a42177f
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-30 17:30:54 -0300

    Filtrar maior string

--- classe-02/index.js
+++ classe-02/index.js
@@ -1,1 +1,5 @@
 const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+
+const maiorString = cidades.reduce((acc, cur) => acc.length > cur.length ? acc : cur);
+
+console.log(maiorString);
