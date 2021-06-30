0d66fe6b1a9b552456c1ce76b34d1f83ef786b03
Rodrigo Lima <rodrigolima.pfg@gmail.com>
2021-06-30 17:49:08 -0300

    Validando lista de compras

--- classe-05/index.js
+++ classe-05/index.js
@@ -1,1 +1,9 @@
 const palavras = ["arroz", "feijão", "carne", "cerveja", "macarrão"];
+
+const temVendaProibida = palavras.some(x => x === "cerveja" || x === "vodka");
+
+if (temVendaProibida) {
+    console.log("revise sua lista, joão. Possui bebida com venda proibida!");
+} else {
+    console.log("tudo certo, vamos as compras!");
+}
